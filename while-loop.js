a8b92886b9c1acddd23af7db8b42916e61e1eb6a
Jorge de la Puente <delapuentem@icloud.com>
2022-08-14 23:31:25 +0200

    Add while loop notes

---
+++ while-loop.js
@@ -0,0 +1,17 @@
+// While loop structure
+
+let i = 0;
+
+while (i <= 10) {
+    console.log(i);
+    i = i + 1;
+}
+
+// Do While structure -> Always do the first run even if condition in the while parenthesis is not true
+
+i = 0;
+
+do {
+    console.log(i);
+    i = i + 1;
+} while (i <= 10)
