16d8d769363ad10238a65933c2a997361a316cae
Jorge de la Puente <delapuentem@icloud.com>
2022-08-30 16:30:24 +0200

    Add ternary conditions notes

---
+++ conditions/conditions-ternary.js
@@ -0,0 +1,21 @@
+// Example 1
+
+const isMember = true
+
+console.log(`You may pay ${(isMember ? '5.00€' : '10.00€')}`)
+
+// Example 2
+const age = 25
+const isSenior = age > 60 ? "Is senior" : "is Junior"
+
+console.log(isSenior)
+
+// Example 3
+
+const animal = 'Dog'
+const isDog = animal === 'Parrot' ? "Is a parrot" : animal === 'Dog' ? "Is a dog" : "Its possible is a cat"
+
+console.log(isDog)
+
+// More examples
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
