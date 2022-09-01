aa64c380e8fb0deb550c698b8604c3946c312dd3
Jorge de la Puente <delapuentem@icloud.com>
2022-09-01 19:09:15 +0200

    Add eslint integration

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log('Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo')
+
+// ESLINT CONFIGURATION
+// install eslint: npm i -D eslint 
+// generate eslint config file: npm init @eslint/config
