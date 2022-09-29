b14370db8c5e8ade2cae592627440838b92d46f8
Jorge de la Puente <59626438+delapuentem@users.noreply.github.com>
2022-09-29 09:34:01 +0200

    Update error-handling-1.js

--- error handling/error-handling-1.js
+++ error handling/error-handling-1.js
@@ -17,3 +17,25 @@ catch(error) {
 finally {
     console.log("This always execute, and is optional")
 }
+
+
+
+const miFuncion1 = function(string){
+    try{
+        if (string === undefined){ 
+            throw new Error(`No has asignado ningún valor al parámetro <string>`)
+        }
+        if ((typeof string) !== 'string'){
+            throw new Error(`El valor debe de ser de tipo string. ${string} es tipo ${typeof string}.`)
+        }
+        return string.length
+
+    }
+    catch(error){
+        return `${error.name}: ${error.message}`
+    }
+}
+
+console.log(miFuncion1())
+console.log(miFuncion1(5))
+console.log(miFuncion1({'nombre': 'Jorge', 'apellidos': 'de la Puente'}))
